d49ba2ba37df0ed9df82d8cbd7df153798da27cc
Dimitrijus Bublys <72878704+DimitrijusBublys@users.noreply.github.com>
2022-04-18 23:19:38 +0300

    Add files via upload

---
+++ LD6/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Encrypt
+{
+    internal class EncryptPassword
+    {
+        public string originalPw;
+        public int lengthPw;
+        private static byte[] IV = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    
+        public EncryptPassword(string password, int passwordLength){
+            originalPw = password;
+            lengthPw = passwordLength;
+        }
+
+        public string Encryption(string placeholder, string originalPw, byte[] IV)
+        {
+            byte[] Key = Encoding.UTF8.GetBytes(originalPw);
+            AesManaged aes = new AesManaged();
+            aes.Key = Key;
+            aes.IV = IV;
+
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+            byte[] InputBytes = Encoding.UTF8.GetBytes(placeholder);
+            cryptoStream.Write(InputBytes,0, InputBytes.Length);
+            cryptoStream.FlushFinalBlock();
+
+            byte[] Encrypted = memoryStream.ToArray();
+
+            Console.WriteLine(Encrypted);
+
+            return Convert.ToBase64String(Encrypted);
+            
+        }
+    }
+}
---
+++ LD6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Encrypt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "";
+            int passwordLength;
+            while(String.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Your password: ");
+                password = Console.ReadLine();
+            }
+            passwordLength = password.Length;
+            EncryptPassword obj = new EncryptPassword(password, passwordLength);
+        }
+    }
+}
